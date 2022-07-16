601d079a0ce05f94831cca7f2a02f6b445981468
Bao Ho <giabaoho123@gmail.com>
2022-07-16 14:57:46 -0700

    check-point

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,19 +2,18 @@ import Navbar from './components/Navbar.js'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
 import Contact from './pages/Contact'
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <><Navbar /><Router>
-      <div>
+    <><Navbar />
+      <Router>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/resume' element={<Resume />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
-      </div>
-    </Router></>
+      </Router></>
   );
 }
 
